d2dcd11a5e447e3cdd45c9fb20e6088c43ac2335
Luis Cáceres <lacc97@protonmail.ch>
2023-08-05 22:35:17 +0000

    Rewrite ObjectPool to use std.heap.MemoryPool

--- src/object_pool.zig
+++ src/object_pool.zig
@@ -6,43 +6,37 @@ pub fn ObjectPool(comptime T: type) type {
     return struct {
         const Self = @This();
 
+        const List = std.SinglyLinkedList(T);
+        const Pool = std.heap.MemoryPool(List.Node);
+
         // --- Fields ---
 
-        arena: std.heap.ArenaAllocator,
-
-        list: std.TailQueue(T) = .{},
-        freelist: std.TailQueue(T) = .{},
+        pool: Pool,
+        list: List = .{},
 
         // --- Public functions ---
 
         pub fn init(allocator: std.mem.Allocator) Self {
-            return .{
-                .arena = std.heap.ArenaAllocator.init(allocator),
-            };
+            return .{ .pool = Pool.init(allocator) };
         }
         pub fn deinit(self: *Self) void {
-            self.arena.deinit();
+            self.pool.deinit();
         }
 
         pub fn alloc(self: *Self) !*T {
-            const node_ptr = get_node: {
-                if (self.freelist.pop()) |n| break :get_node n;
-                const n = try self.arena.allocator().create(std.TailQueue(T).Node);
-                break :get_node n;
-            };
-
-            self.list.append(node_ptr);
+            const node_ptr = try self.pool.create();
+            self.list.prepend(node_ptr);
             return &node_ptr.data;
         }
         pub fn dealloc(self: *Self, obj: *T) void {
-            const node_ptr = @fieldParentPtr(std.TailQueue(T).Node, "data", obj);
+            const node_ptr = @fieldParentPtr(List.Node, "data", obj);
             self.list.remove(node_ptr);
-            self.freelist.append(node_ptr);
+            self.pool.destroy(node_ptr);
         }
 
         pub fn next(self: *Self, obj: ?*T) ?*T {
             const next_node = get_next: {
-                if (obj) |o| break :get_next @fieldParentPtr(std.TailQueue(T).Node, "data", o).next;
+                if (obj) |o| break :get_next @fieldParentPtr(List.Node, "data", o).next;
                 break :get_next self.list.first;
             } orelse return null;
             return &next_node.data;
